10ee219f2e21ac2f80fa5d71177453adcf0df029
Daniel <subiabrewd@gmail.com>
2023-11-13 19:57:08 +0100

    Test uniqueness against concurrency in a 100 batches of a 100 ids each

--- tests/SnowflakeGeneratorTest.php
+++ tests/SnowflakeGeneratorTest.php
@@ -2,35 +2,29 @@
 
 namespace Subiabre\Tests;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Subiabre\SnowflakeGenerator;
 use PHPUnit\Framework\TestCase;
 
 class SnowflakeGeneratorTest extends TestCase
 {
-    /**
-     * @var SnowflakeGenerator
-     */
-    private $snowflake;
+    public static function provideIds(): array
+    {
+        $array = array_fill(0, 100, null);
+        $generator = new SnowflakeGenerator();
 
-    public function setUp(): void{
-        $this->snowflake = new SnowflakeGenerator;
+        return array_fill(0, 100, [array_map(function() use ($generator) {
+            return $generator->new();
+        }, $array)]);
     }
 
-    public function testExplicitlyGeneratesId()
+    #[DataProvider('provideIds')]
+    public function testNotConcurrent($ids)
     {
-        $id1 = $this->snowflake->getId();
-        $id2 = $this->snowflake->getId();
-        $id3 = $this->snowflake->new();
+        $this->assertCount(100, $ids);
 
-        $this->assertSame($id1, $id2);
-        $this->assertNotSame($id1, $id3);
-    }
-
-    public function testInstanceToString()
-    {
-        $instance = $this->snowflake->getId();
-        $string = (string) $this->snowflake;
-
-        $this->assertSame($instance, $string);
+        foreach ($ids as $id) {
+            $this->assertEquals(1, count(array_keys($ids, $id)));
+        }
     }
 }
