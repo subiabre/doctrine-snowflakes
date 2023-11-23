1d3f9ac49ab9b4cddef4b414d1e38fac109378ac
Daniel Subiabre <subiabrewd@gmail.com>
2023-11-23 22:34:30 +0100

    Add comment explaining location of LockDirTest

--- tests/LockDirectoryTest/LockDirectoryTest.php
+++ tests/LockDirectoryTest/LockDirectoryTest.php
@@ -7,7 +7,11 @@ use Subiabre\SnowflakeGenerator;
 
 class LockDirectoryTest extends TestCase
 {
-    public function testLockDirectoryIsAtProjectRoot()
+    /*
+        This test is located at a different folder to make sure PHP builds path at project root
+        regardless of the path at which `SnowflakeGenerator::__construct()` is called at
+    */
+    public function testLockdirDefaultPathIsAtProjectRoot()
     {
         $generator = new SnowflakeGenerator();
         $lockdir   = sprintf('%s%s%s', \dirname(__DIR__, 2), DIRECTORY_SEPARATOR, 'lock');
